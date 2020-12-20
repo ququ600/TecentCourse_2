31dd099db3b60929decadefa7c53660a56d4f3f8
ququ600 <1329447051@qq.com>
2020-12-20 16:50:41 +0800

    第六次作业

---
+++ Source/TecentCourse_2/Public/Ak47.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gun.h"
+#include "Ak47.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TECENTCOURSE_2_API AAk47 : public AGun
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TecentCourse_2/Public/SMG.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gun.h"
+#include "SMG.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TECENTCOURSE_2_API ASMG : public AGun
+{
+	GENERATED_BODY()
+	
+};
